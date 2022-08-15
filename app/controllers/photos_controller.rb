7ac57ab221cbfc8d90f35c6bc2d605558ce3e7b8
dylansarikas <dylan.sarikas@gmail.com>
2022-08-15 17:47:45 -0500

    adds photos create

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,18 @@
 class PhotosController < ApplicationController
     def index
-        photos = Photos.all
+        photos = Photo.all
         render json: photos.as_json
     end
+
+    def create
+        photo = Photo.new(
+            name = params[:name],
+            width = params[:width],
+            height = params[:height]
+        )
+        photo.save
+        render json: photo.as_json
+    end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
   #Hello
   get "/photos" => "photos#index"
+  post "/photos" => "photos#create"
 end
